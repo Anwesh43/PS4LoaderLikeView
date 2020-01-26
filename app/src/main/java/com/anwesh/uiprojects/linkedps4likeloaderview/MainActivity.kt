b373f93d4837a27412f82f2e2fdf18ac38fa0de4
Anwesh43 <anweshthecool0@gmail.com>
2020-01-26 11:41:48 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedps4likeloaderview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedps4likeloaderview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedps4likeloaderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ps4likeloaderview.PS4LoaderLikeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PS4LoaderLikeView.create(this)
     }
 }
