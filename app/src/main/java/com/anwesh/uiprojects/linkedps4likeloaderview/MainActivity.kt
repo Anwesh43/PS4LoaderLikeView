85bf54a8938e729831b0b0a27d39e6b53e377f26
Anwesh43 <anweshthecool0@gmail.com>
2020-01-26 11:43:23 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedps4likeloaderview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedps4likeloaderview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedps4likeloaderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.ps4likeloaderview.PS4LoaderLikeView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PS4LoaderLikeView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
